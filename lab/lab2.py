a06a780b08f80240d133c0a4f6c5cb40afc890be
Muhammadabubakar503 <70576366+Muhammadabubakar503@users.noreply.github.com>
2020-09-02 03:29:07 +0500

    Add files via upload

---
+++ lab/lab2.py
@@ -0,0 +1,42 @@
+student_1_data = {"name": "abubakar", "roll number": "bdsm-s20-003", "study": "bs data science", "age": "20",
+                  "semester": "3nd"}
+student_2_data = {"name": "hamza", "roll number": "bdsm-s20-001", "study": "bs science", "age": "21",
+                  "semester": "6nd"}
+student_3_data = {"name": "ali", "roll number": "bdsm-s20-004", "study": "bs computer science", "age": "22",
+                  "semester": "4nd"}
+student_4_data = {"name": "zeeshan", "roll number": "bdsm-s20-005", "study": "bs data science", "age": "24",
+                  "semester": "8nd"}
+student_5_data = {"name": "usman", "roll number": "bdsm-s20-002", "study": "bs computer science", "age": "29",
+                  "semester": "5nd"}
+liststudent = [student_1_data, student_2_data, student_3_data, student_4_data, student_5_data]
+print(liststudent)
+print(">>>>>>>>>>>>>>>>>>>>>>")
+print(liststudent[0]["name"])
+print(liststudent[1]["name"])
+print(liststudent[2]["name"])
+print(liststudent[3]["name"])
+print(liststudent[4]["name"])
+print(">>>>>>>>>>>>>>>>>>>>>>")
+print(liststudent[0]["roll number"])
+print(liststudent[1]["roll number"])
+print(liststudent[2]["roll number"])
+print(liststudent[3]["roll number"])
+print(liststudent[4]["roll number"])
+print(">>>>>>>>>>>>>>>>>>>>>>")
+print(liststudent[0]["study"])
+print(liststudent[1]["study"])
+print(liststudent[2]["study"])
+print(liststudent[3]["study"])
+print(liststudent[4]["study"])
+print(">>>>>>>>>>>>>>>>>>>>>>")
+print(liststudent[0]["age"])
+print(liststudent[1]["age"])
+print(liststudent[2]["age"])
+print(liststudent[3]["age"])
+print(liststudent[4]["age"])
+print(">>>>>>>>>>>>>>>>>>>>>>")
+print(liststudent[0]["semester"])
+print(liststudent[1]["semester"])
+print(liststudent[2]["semester"])
+print(liststudent[3]["semester"])
+print(liststudent[4]["semester"])
