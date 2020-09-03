3d87accc435fba91a7796d755f683ea9209ce6fc
Muhammadabubakar503 <70576366+Muhammadabubakar503@users.noreply.github.com>
2020-09-03 09:02:34 +0500

    Add files via upload

--- lab3_task1_table_oop.py
+++ lab3_task1_table_oop.py
@@ -3,19 +3,19 @@ print("...................")
 print("Table using while loop")
 print("...................")
 num= int(input("enter table number : "))
-
+y=int(input("enter the range : "))
 i = 1
-while i <= 10:
+while i <= y:
         x = num*i
-        print(x)
+        print(num ,"*",i, "=", x)
         i = i + 1
 #printing table using while loop
 print("...................")
 print("Table using for loop")
 print("...................")
 num= int(input("enter table number : "))
-
+y=int(input("enter the range : "))
 i=1
-for i in range(1,11):
+for i in range(1,y+1):
     x=num*i
-    print(x)
+    print(num, "*", i, "=", x)
