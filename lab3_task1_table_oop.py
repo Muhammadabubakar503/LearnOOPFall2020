3ce65c6218471a3ebfea32004550eb689286ad64
Muhammadabubakar503 <70576366+Muhammadabubakar503@users.noreply.github.com>
2020-09-03 09:14:07 +0500

    Add files via upload

--- lab3_task1_table_oop.py
+++ lab3_task1_table_oop.py
@@ -2,20 +2,22 @@
 print("...................")
 print("Table using while loop")
 print("...................")
-num= int(input("enter table number : "))
-y=int(input("enter the range : "))
-i = 1
-while i <= y:
-        x = num*i
-        print(num ,"*",i, "=", x)
-        i = i + 1
+number= int(input("enter table number : "))
+lower_range=int(input("enter the lower range : "))
+upper_range=int(input("enter the upper range : "))
+
+while lower_range <= upper_range :
+    product = number*lower_range
+    print(number ,"*",lower_range, "=", product)
+    lower_range += 1
 #printing table using while loop
 print("...................")
 print("Table using for loop")
 print("...................")
-num= int(input("enter table number : "))
-y=int(input("enter the range : "))
-i=1
-for i in range(1,y+1):
-    x=num*i
-    print(num, "*", i, "=", x)
+number= int(input("enter table number : "))
+lower_range=int(input("enter the lower range : "))
+upper_range=int(input("enter the upper range : "))
+
+for lower_range in range(lower_range,upper_range+1):
+    product = number*lower_range
+    print(number ,"*",lower_range, "=", product)
