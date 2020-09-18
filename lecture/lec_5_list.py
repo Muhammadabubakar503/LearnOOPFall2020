898e39241dc747e0f775216465066150f59e2e0e
Muhammadabubakar503 <70576366+Muhammadabubakar503@users.noreply.github.com>
2020-09-18 17:00:47 +0500

    Add files via upload

---
+++ lecture/lec_5_list.py
@@ -0,0 +1,13 @@
+# list in python
+list1=[1,2,3,4,5,]
+list2=list1
+print(list1)
+print(list2)
+print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+def re_arrange(l):
+    number=[]
+    for i in range (len(ss)):
+        number.append(ss.pop())
+    return number
+ss=list(range(1,7))
+print(re_arrange(ss))  
